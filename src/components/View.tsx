19e01ba3dd686b6ce404d785626a1d7c456e1571
Mohammad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-11-17 01:00:38 +0330

    View component was used in App and some style was applied to thata

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,7 +1,19 @@
 import classes from './View.module.css';
 
-const View = () => (
-    <div>this code</div>
+interface IProps {
+    names: string;
+    age: number;
+}
+
+const View = ({ names, age }: IProps) => (
+    <div className={classes.stack}>
+        <div>
+            {names}
+        </div>
+        <div>
+            {age}
+        </div>
+    </div>
 );
 
 export default View;
