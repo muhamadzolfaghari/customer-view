3f9c87b66c8e7c5995352801677dfdf1fdcb22a2
Mohammad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-11-17 13:50:00 +0330

    View component design was completed and test was added correctly to check the component was defined correctly

--- src/__tests__/View.test.tsx
+++ src/__tests__/View.test.tsx
@@ -3,13 +3,9 @@ import View from "../components/View"
 
 test("Render View", async () => {
     render(<View names={"John"} age={12} />);
-
     // This code is return the number of row that is existed
-    const rows = await screen.findAllByRole("row");
-    expect(rows.length).toBe(2);
-
-    expect(screen.getByText(/age:/i));
-    expect(screen.getByText(/name:/i));
-    expect(screen.getByText(/John/i));
-    expect(screen.getByText(/12/i));
+    expect(await screen.findAllByRole("row")).toHaveLength(2);
+    expect(screen.getByText(/age:/i)).toBeDefined();
+    expect(screen.getByText(/john/i)).toBeDefined();
+    expect(screen.getByText(/12/i)).toBeDefined();
 })
--- src/components/View.tsx
+++ src/components/View.tsx
@@ -8,11 +8,11 @@ interface IProps {
 const View = ({ names, age }: IProps) => (
     <div className={classes.view}>
         <img src={"./user.png"} className={classes.avatar} />
-        <div className={classes['view-row']} role="row">
-            <span>name:</span><span>{names}</span>
+        <div className={classes.row} role="row">
+            <h3 className={classes.name} role='name'>{names}</h3>
         </div>
-        <div className={classes['view-row']} role="row">
-            <span>age:</span><span>{age}</span>
+        <div className={classes.row} role="row">
+            <span className={classes['item-key']}>age:</span><span>{age}</span>
         </div>
     </div >
 );
