9c2b0a17b4923cf275c72bb5f88920ecc6212d9e
Mohammad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-11-17 22:03:09 +0330

    some changes were applied to view design and comments added to useCustomerQuery hook.

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,19 +1,20 @@
 import classes from './View.module.css';
 
 interface IProps {
-    names: string;
-    age: number;
+  names: string;
+  age: number;
 }
 
 const View = ({ names, age }: IProps) => (
-    <div className={classes.view}>
-        <div className={classes.row} role="row">
-            <h2 className={classes.name} role='name'>{names}</h2>
-        </div>
-        <div className={classes.row} role="row">
-            <span className={classes['item-key']}>age:</span><span className={classes.age}>{age}</span>
-        </div>
-    </div >
+  <div className={classes.view}>
+    <div className={classes.row} role="row">
+      <h2 className={classes.name} role='name'>{names}</h2>
+    </div>
+    <div className={classes.row} role="row">
+      <span className={classes['item-key']}>age:</span>
+      <span className={classes.age}>{age}</span>
+    </div>
+  </div>
 );
 
 export default View;
